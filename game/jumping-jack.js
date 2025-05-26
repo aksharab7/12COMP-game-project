92eac337fdb249a0bbbd9bd46832fdad4e790b5f
aksharab7 <aksharab712@gmail.com>
2025-05-26 11:10:01 +0000

    SAVEE

--- game/jumping-jack.js
+++ game/jumping-jack.js
@@ -1,20 +1,29 @@
+
+
 /*******************************************************/
 // Project: Create a Game
 // Jumping-Jack
 /// Written by Akshara
 /*******************************************************/
 
+
 /*******************************************************/
 // Variables
 /*******************************************************/
 const PLATFORMWIDTH = 110;
 const PLATFORMHEIGHT = 20;
-const JACKWIDTH = 70;
+const JACKWIDTH = 10;
 const JACKHEIGHT = 70;
 const PLATFORMMIN = -9;
 const PLATFORMMAX= 10;
 
-var wallGroup, platformGroup, wallLH, wallRH, wallBot;
+
+var jack, platformGroup, wallBot;
+let isJumping = false;
+let jumpForce = -12;
+let gravity = 0.5;
+
+
              
 /*******************************************************/
 // preload()
@@ -26,6 +35,8 @@ function preload() {
    jackLeftImg = loadImage('/images/jumping-jack-left.png');
    platformImg = loadImage('/images/platform.png');
   }
+
+
 
 
 /*******************************************************/
@@ -45,8 +56,15 @@ function setup() {
 /*******************************************************/
 function draw() {
   clear();       // clears screen before each frame
-  moveJack();    // update Jack's position and controls
+  moveJack(); 
+  
+camera.x = jack.x; // set camera to Jack's x position
+wallBot.x = jack.x;
+camera.on();
+jack.draw(); // draw Jack
+camera.off();
   }
+
 
 /*******************************************************/
 // createJack()
@@ -55,10 +73,12 @@ function draw() {
 /*******************************************************/
 function createJack(){
  console.log("createJack()");
- jack = createSprite(width/2, height - JACKHEIGHT, JACKWIDTH, JACKHEIGHT, 'k');
+ jack = createSprite(100, height - JACKHEIGHT, JACKWIDTH, JACKHEIGHT, 'k');
  jack.img = (jackRightImg);
  jackRightImg.resize(100, 120);
+ jack.vel.x = 1.5;// update Jack's position and controls
 }
+
 
 /*******************************************************/
 // moveJack()
@@ -66,15 +86,18 @@ function createJack(){
 // Called by draw()
 /*******************************************************/
 function moveJack(){
-if (kb.pressing("left")) {
+  console.log("moveJack()");
+  // Move left/right
+  if (kb.pressing("left")) {
    jack.x -= 5;
    jack.img = jackLeftImg;
    jackLeftImg.resize(100, 120);
- } else if (kb.pressing("right")) {
-   jack.x += 5;
-     jack.img = jackRightImg;
-   }
- }
+  }
+  if (kb.pressing("space")) {
+    jack.y = -5;
+    }
+}
+
 
 /*******************************************************/
 // createPlatfroms()
@@ -82,53 +105,47 @@ if (kb.pressing("left")) {
 // Called by setup()
 /*******************************************************/
 function createPlatfroms() {
+  console.log("createPlatfroms()");
   platformGroup = new Group ();
-  platformGroup.collides(wallGroup, platformHitWall);  // bounce off walls
+  let numPlatforms = 20;
+  let verticalSpacing = height / (numPlatforms + 1);
 
- for ( i = 0; i < 10; i++) {
-   var xPos = random(width - PLATFORMWIDTH);  // random horizontal position
-   var yPos = height - i * 70;                // vertical position
-   platform = new Sprite(xPos, yPos, PLATFORMWIDTH, PLATFORMHEIGHT, 'd');
-   platform.img =  (platformImg);
-   platformImg.resize(120, 40);
-   platform.vel.x = random(PLATFORMMIN, PLATFORMMAX);  // velocity for each sprite
-   platform.vel.y = 0;
-   platformGroup.add(platform);
-   platform.rotation.vel = 0;
-   platform.rotation = 0;
-   platform.add;
+
+  for (i = 0; i < numPlatforms; i++) {
+  let posX = random(PLATFORMWIDTH / 2, width - PLATFORMWIDTH / 2); // Random X within screen bounds
+  let posY = height - (i + 1) * verticalSpacing; // Even vertical spacing
+  platform = new Sprite(posX, posY, PLATFORMWIDTH, PLATFORMHEIGHT, "s");
+  platform.img =  (platformImg);
+  platformImg.resize(120, 40);
+  platform.vel.x = random(-5,10)  // velocity for each sprite
+  platform.vel.y = 0;
+  platformGroup.add(platform);
+  platform.add;
 }
 
+
 }
+
 
 /*******************************************************/
 // createWalls()
-// Creates invisible walls on left, right, and bottom
+// Creates walls on left, right, and bottom
 // Called by setup()
 /*******************************************************/
  function createWalls(){
-    wallGroup = new Group ();
-    // Left Wall
-    wallLH  = new Sprite(0, height/2, 8, height, 'k');
-    wallLH.color = 'blue';
-    wallGroup.add(wallLH);
-    // Right Wall
-    wallRH  = new Sprite(width, height/2, 8, height, 'k');
-    wallRH.color = 'blue';
-    wallGroup.add(wallRH);
-    // Bottom Wall
+    console.log("createWalls()");
+    // Bottom Wall 
     wallBot = new Sprite(width ,height , 2 * width, 8, 'k');
     wallBot.color = 'green';
 }
 
-/*******************************************************/
-// platformHitWall()
+
+/*******************************************************
+// PlatformHitWall()
 // Repositions platform when it hits left/right wall
 // Called by createPlatfroms()
 /*******************************************************/
-function platformHitWall(platform, wallRH) {
-  platform.x = random(PLATFORMWIDTH/2, width-PLATFORMWIDTH/2);
-  platform.y = random(PLATFORMHEIGHT/2, 2 * PLATFORMHEIGHT);
-  platform.vel.x = random(PLATFORMMIN, PLATFORMMAX);
-  platform.vel.y = 0;
-}
+
+
+
+
