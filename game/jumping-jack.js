a91c47afb10f8a4ac3e271aee4a9f81686f02c42
aksharab7 <aksharab712@gmail.com>
2025-05-31 04:30:24 +0000

    Acc done

--- game/jumping-jack.js
+++ game/jumping-jack.js
@@ -13,11 +13,9 @@ const PLATFORMWIDTH = 80;
 const PLATFORMHEIGHT = 20;
 const TIMELIMIT = 30;
 
-var jack, platformGroup, wallBot;
-var lastY;
-
-let seconds = TIMELIMIT;
+let jack, platformGroup, wallBot;
 let timer;
+let seconds = TIMELIMIT;
 let score = 0;
 
 /*******************************************************/
@@ -26,6 +24,7 @@ let score = 0;
 /*******************************************************/
 function preload() {
   console.log("preload:");
+
   jackRightImg = loadImage('/images/jumping-jack-right.png');
   jackLeftImg = loadImage('/images/jumping-jack-left.png');
   platformImg = loadImage('/images/platform.png');
@@ -37,18 +36,21 @@ function preload() {
 /*******************************************************/
 function setup() {
   console.log("setup:");
+
+  // Set up the canvas and world
   createCanvas(windowWidth, windowHeight - 5);
   world.gravity.y = 20;
 
-  createJack();             
-  createPlatforms();   
+  createJack();
+  createPlatforms();
+  html_listen4Debug();
 
-  wallBot = new Sprite(0, height, 2 * width, 20, 'k'); // Create bottom wall
+  //creating the bottom wall
+  wallBot = new Sprite(0, height, 2 * width, 20, 'k'); 
   wallBot.color = 'green';
 
-  lastY = jack.y;
-
-  timer = setInterval(timerFunc, 1000); // Start countdown timer
+  // Start countdown timer
+  timer = setInterval(timerFunc, 1000); 
 }
 
 /*******************************************************/
@@ -60,29 +62,35 @@ function draw() {
   moveJack();
   updateScore();
   drawScore();
-  camera.y = jack.y; // Camera follows Jack vertically
+
+  // Camera follows Jack vertically
+  camera.y = jack.y;
 }
 
 /*******************************************************/
 // createJack() 
 // Create Jack sprite 
 // Called by setup ()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function createJack() {
+  console.log("createJack:");
+
   jack = createSprite(width / 2, height - JACKHEIGHT, JACKWIDTH, JACKHEIGHT, 'd');
   jack.img = jackRightImg;
   jackRightImg.resize(100, 120);
   jack.image.offset.y = -10;
   jack.image.offset.x = 4;
   jack.rotationLock = true;
-  jack.debug = true;
-
 }
 
 /*******************************************************/
 // moveJack()
 // Jack's movement and jumping
 // Called by draw()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function moveJack() {
   if (kb.pressing("left")) {
@@ -98,39 +106,44 @@ function moveJack() {
     jack.image.offset.y = -10;
     jack.image.offset.x = 4;
   }
-
-  if (kb.presses("space")) {
-      jack.vel.y = -10;
-    }
   
+  // Jump if space is pressed
+  if (kb.presses("space")) {
+    jack.vel.y = -10;
+  }
 }
 
 /*******************************************************/
 // createPlatforms()
 // Create multiple platforms
 // Called by setup()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function createPlatforms() {
-  platformGroup = new Group();
+  console.log("createPlatforms:");
 
-  for (let i = 0; i < 100; i++) {
-    let posX = random(100, width - 100);
-    let posY = jack.y - i * 90;
+  // Create a group for platforms scattered across the screen
+  platformGroup = new Group();
+  for (let i = 0; i < 300; i++) {
+    let posX = random(100, width - 100);    //random X position
+    let posY = jack.y - i * 80;             //spaced 80px apart vertically
 
     let platform = new Sprite(posX, posY, PLATFORMWIDTH, PLATFORMHEIGHT, "s");
     platform.img = platformImg;
     platformImg.resize(120, 40);
-    platform.image.offset.y = 3
+    platform.image.offset.y = 3;
     platform.image.offset.x = -20;
     platformGroup.add(platform);
   }
-   platformGroup.debug = true; 
 }
 
 /*******************************************************/
 // updateScore() 
 // Score changes with height
 // Called by draw()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function updateScore() {
   if (jack.vel.y < -1) {
@@ -146,11 +159,14 @@ function updateScore() {
 // timerFunc() 
 // Game timer
 // Called by setup()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function timerFunc() {
+  console.log("timerFunc:");
   seconds--;
- 
- if (seconds <= 0) {
+
+  if (seconds <= 0) {
     gameOver();
     clearInterval(timer);
   }
@@ -159,7 +175,9 @@ function timerFunc() {
 /*******************************************************/
 // drawScore() 
 // Show score and time on screen
-// called by draw()
+// Called by draw()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function drawScore() {
   camera.off();
@@ -176,18 +194,66 @@ function drawScore() {
 /*******************************************************/
 // gameOver() 
 // End the game and show final score
-// called by timerFunc()
+// Called by timerFunc()
+// Input:  n/a
+// Return: n/a
 /*******************************************************/
 function gameOver() {
-  noLoop(); 
+  // Stop draw() loop
+  noLoop();
   clearInterval(timer);
-   
-  sessionStorage.setItem("score", score);
 
-  const highScore = sessionStorage.getItem("highScore");
+  // Save score and high score in sessionStorage
+  sessionStorage.setItem("score", score);
+  const highScore = sessionStorage.getItem("highScore") || 0;
   if (score > highScore) {
     sessionStorage.setItem("highScore", score);
   }
 
- window.location.href = "jj_end.html"; 
+  window.location.href = "jj_end.html";
 }
+
+/**************************************************************/
+// html_listen4Debug()
+// Register keyboard keydown event
+// To check if user wants sprite debug mode OR not
+//  Hit ctrl-z to turn debug mode on
+//  Hti ctrl-x to turn debug mode off
+// Input:  n/a
+// Return: n/a
+/**************************************************************/
+function html_listen4Debug() {
+  console.log('%c html_listen4Debug(): ',
+          'color: white; background-color: purple;');
+
+  document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'z') {
+      console.log('%c html_listen4Debug(): ACTIVE',
+          'color: white; background-color: purple;');
+      // Place debug code here ***************************
+      // EG to set debug mode for all sprites 
+      allSprites.debug = true; 
+      
+      // To slow the frame rate down to 1 frame/sec
+      frameRate(1);
+      
+      // OR to tell P5 to NOT call the draw loop again (freeze the screen)
+      //noLoop();
+      
+    }
+    else if (event.ctrlKey && event.key === 'x') {
+      console.log('%c html_listen4Debug(): INACTIVE',
+          'color: white; background-color: purple;');
+      // Place code to turn off debug mode here *********
+      // EG to turn off debug mode for all sprites
+      allSprites.debug = false; 
+      
+      // To reset frame rate to 60 frame/sec
+      frameRate(60);
+      
+      // OR if you set noLoop() - to start P5 calling the draw loop again
+      //loop();
+    }
+  });
+}
+ 
